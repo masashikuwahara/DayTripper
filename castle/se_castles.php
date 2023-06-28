9875cbdcf2a811a876b23f7c2911355f0a48bdd6
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2023-06-29 00:29:51 +0900

    不要ページ削除

--- castle/se_castles.php
+++ castle/se_castles.php
@@ -4,17 +4,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>100名城近畿</title>
+  <link rel="stylesheet" href="../style.css">
+  <title>100名城北海道</title>
 </head>
 <body>
 <?php include('header.php'); ?>
 
-  <h1 class="title">100名城近畿</h1>
+  <h1 class="title">100名城北海道</h1>
 
   ここにforで繰り返しで表示させる
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script type="text/javascript" src="menu.js"></script>
+  <script type="text/javascript" src="../menu.js"></script>
 
   <?php include('footer.php'); ?>
