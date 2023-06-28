5e2a07e9c1f125485bde5c835d232a62dde476de
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2023-06-29 00:29:25 +0900

    100名城ページ作成

--- 100_castles.php
+++ 100_castles.php
@@ -9,19 +9,56 @@
 </head>
 <body>
 <?php include('header.php'); ?>
-
+<div class="header_text">
   <h1 class="title">100名城</h1>
+  <?php
+    try
+    {
+      require('connect.php');
+      $dbh->query('SET NAMES utf8');
+      $sql='SELECT id,title,img1 FROM 100castles ORDER BY cas';
+      $stmt=$dbh->prepare($sql);
+      $stmt->execute();
 
-  <div class="split">
-    <?php echo '<a class="region" href="castle/hokkaido.php">北海道</a>' ?>
-    <?php echo '<a class="region" href="castle/tohoku.php">東北</a>' ?>
-    <?php echo '<a class="region" href="castle/kanto.php">関東</a>' ?>
-    <?php echo '<a class="region" href="castle/chubu.php">中部</a>' ?>
-    <?php echo '<a class="region" href="castle/kinki.php">近畿</a>' ?>
-    <?php echo '<a class="region" href="castle/chugoku.php">中国</a>' ?>
-    <?php echo '<a class="region" href="castle/shikoku.php">四国</a>' ?>
-    <?php echo '<a class="region" href="castle/kyushu.php">九州</a>' ?>
-  </div>
+      $dbh=null;
+
+      echo '日本100名城のページです<br/>'.'いずれ全部行ってみたいです。<br />';
+
+      while(true)
+      {
+        $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+        if($rec==false)
+        {
+          break;
+        }
+
+        if($rec['img1']=='')
+        {
+          $img_name='';
+        }
+        else
+        {
+          $img_name='<img style="width:360px" src="img/'.$rec['img1'].'">';
+        }
+
+        echo '<span class="img_style">'.
+            '<a href="detail.php?id='.$rec['id'].'">'.
+            $img_name.
+            '<br />'.
+            $rec['title'].
+              '</a>'.
+            '<br />'.
+            '</span>';
+      }
+
+      }
+      catch (Exception $e)
+      {
+        echo 'ただいま障害により大変ご迷惑をお掛けしております。';
+        exit();
+      }
+      ?>
+</div>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script type="text/javascript" src="menu.js"></script>
