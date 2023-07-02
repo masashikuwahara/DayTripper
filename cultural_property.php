64ccad102d26991098ac2dad2459664ab8e88c7f
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2023-07-02 13:22:32 +0900

    その他文化財ページ作成

--- cultural_property.php
+++ cultural_property.php
@@ -9,9 +9,62 @@
 </head>
 <body>
 <?php include('header.php'); ?>
-
+<div class="castle_page">
   <h1 class="title">その他文化財</h1>
+  
+  <div class="about">
+    城周辺、もしくはその道中には様々な文化財や昔の暮らしの名残みたいなのが残っています。
+    その中で気になったものをまとめてみました。
+    また100名城、続100名城以外の城もこちらにまとめてあります。
+  </div>
+  <?php
+    try
+    {
+      require('connect.php');
+      $dbh->query('SET NAMES utf8');
+      $sql='SELECT id,title,img1 FROM cultures ';
+      $stmt=$dbh->prepare($sql);
+      $stmt->execute();
 
-  ここにforで繰り返しで表示させる
+      $dbh=null;
+
+      while(true)
+      {
+        $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+        if($rec==false)
+        {
+          break;
+        }
+
+        if($rec['img1']=='')
+        {
+          $img_name='';
+        }
+        else
+        {
+          $img_name='<img style="width:360px" src="img/'.$rec['img1'].'">';
+        }
+
+        echo '<span class="img_style">'.
+            '<a href="culturals.php?id='.$rec['id'].'">'.
+            $img_name.
+            '<br />'.
+            $rec['title'].
+              '</a>'.
+            '<br />'.
+            '</span>';
+      }
+
+      }
+      catch (Exception $e)
+      {
+        echo 'ただいま障害により大変ご迷惑をお掛けしております。';
+        exit();
+      }
+      ?>
+</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="menu.js"></script>
 
   <?php include('footer.php'); ?>
