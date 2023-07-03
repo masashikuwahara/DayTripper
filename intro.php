9d663c829dc0149eaac23b84f304686442ad78bc
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2023-07-03 22:15:03 +0900

    はじめにページ修正

--- intro.php
+++ intro.php
@@ -11,29 +11,35 @@
 <?php include('header.php'); ?>
 
   <h1 class="title">はじめに</h1>
+  <div class="wrapper">
+    <main class="main">
+      <img src="img/001.jpg" class="main_img" alt="image">
+    </main>
+    <aside class="side">
+      <h2 class="intro">このページを制作した理由について</h2>
+      <p class="intro">
+        DayTripperの記録は私がこれまで旅してきた場所、主に100名城、続100名城を中心に旅してきました。
+        まあせっかくなので何らかの形で残そうと思いこのようなwebページを作成しました。
+        100名城、続100名城、その他文化財とカテゴリーを分けましたのでお好きなページからご覧ください。
+        一応まだまだ訪れる場所はあるので今後増えると思います。その都度アップしていく予定です。
+        もし時間があればよければ見てやってください。
+      </p>
+    </aside>
+  </div>
 
-  <h2>このページを制作した理由について</h2>
-  <p class="intro">
-    DayTripperの記録は私がこれまで旅してきた場所、主に100名城、続100名城を中心に旅してきました。
-    まあせっかくなので何らかの形で残そうと思いこのようなwebページを作成しました。
-    100名城、続100名城、その他文化財とカテゴリーを分けましたのでお好きなページからご覧ください。
-    一応まだまだ訪れる場所はあるので今後増えると思います。その都度アップしていく予定です。
-    もし時間があればよければ見てやってください。
-  </p>
-  
-  <H2>
+  <!-- <h2>
     旅の持ち物
-  </H2>
-  <P class="intro">
+  </h2>
+  <P class="article">
     ここに普段の持ち物、写真を載せる
   </P>
 
-  <H2>
+  <h2>
     旅の心得？
-  </H2>
-  <P class="intro">
+  </h2>
+  <P class="article">
     ここに文章
-  </P>
+  </P> -->
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script type="text/javascript" src="menu.js"></script>
