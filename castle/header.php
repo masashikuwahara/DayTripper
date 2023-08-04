2a11d1b552610f1ec42255c4a2e1412d02baebfd
masashikuwahara <80254228+masashikuwahara@users.noreply.github.com>
2023-08-05 08:48:35 +0900

    commit

--- castle/header.php
+++ castle/header.php
@@ -12,6 +12,7 @@
     <div class="menu">
       <ul>
         <li><a href="../intro.php">はじめに</a></li>
+        <li><a href="../genzon.php">現存天守</a></li>
         <li><a href="../100_castles.php">100名城</a></li>
         <li><a href="../sequel_100_castles.php">続100名城</a></li>
         <li><a href="../cultural_property.php">その他文化財</a></li>
---
+++ genzon.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>現存天守</title>
+</head>
+<body>
+<?php include('header.php'); ?>
+<div class="castle_page">
+  <h1 class="title">現存天守</h1>
+  
+  <div class="about">
+    現存天守とは江戸時代以前に建造され、現在でも残っている天守のことを言います。
+    かつて日本全国には多数の城があり、天守閣も多くありました。しかし戦乱、江戸時代初期の
+    一国一城令、明治維新の廃城令、戦争での焼失、落雷や家事での焼失などの理由により
+    ほとんどが失われ、現在では12城が残るのみです。現存天守以外の天守閣は復元されたものです。
+    自分はこれまで8か所訪れましたのでご紹介します。
+  </div>
+  <hr>
+
+<div class="container">
+  <div class="grid_2">
+    <a href="detail.php?id=20"><img src="img/himejijou.jpg" width="40%" alt="姫路城"></a>
+    <p>城名</p>
+  </div>
+  <div class="grid_2">
+    <a href="detail.php?id=20"><img src="img/himejijou.jpg" width="40%" alt="姫路城" class="mr1"></a>
+    <p>城名</p>
+  </div>
+  <div class="grid_2">
+    <a href="detail.php?id=20"><img src="img/himejijou.jpg" width="40%" alt="姫路城"></a>
+    <p>城名</p>
+  </div>
+</div>
+
+</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="menu.js"></script>
+
+  <?php include('footer.php'); ?>
