7b72134b463928f4a37b4cf5d002895bba0dd955
masashikuwahara <brucemclarenp4@gmail.com>
2023-10-27 00:29:08 +0900

    リンク、レイアウト設定

--- japan_map.php
+++ japan_map.php
@@ -6,18 +6,61 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <title>都道府県別</title>
+  <style>
+    #title {
+      text-align: center;
+      margin-top: 80px;
+    }
+  </style>
 </head>
-<style>
-  #title{
-    margin-top: 70px;
-  }
-</style>
 <body>
 <?php include('header.php'); ?>
 
 <h1 id=title>都道府県別</h1>
 
 <h2 id=hokkaido>
+  <span class="img_style">
+    <a href="detail.php?id=18"><img style="width:360px" src="img/960px-Nemurohanto_chashiatogun_onnemotochashi.jpg"><br />根室半島チャシ跡群</a>
+  </span>
+  <span class="img_style">
+    <a href="detail.php?id=18"><img style="width:360px" src="img/960px-Nemurohanto_chashiatogun_onnemotochashi.jpg"><br />根室半島チャシ跡群</a>
+  </span>
+  <span class="img_style">
+    <a href="detail.php?id=18"><img style="width:360px" src="img/960px-Nemurohanto_chashiatogun_onnemotochashi.jpg"><br />根室半島チャシ跡群</a>
+  </span>
+  <span class="img_style">
+    <a href="detail.php?id=18"><img style="width:360px" src="img/960px-Nemurohanto_chashiatogun_onnemotochashi.jpg"><br />根室半島チャシ跡群</a>
+  </span>
+</h2>
+
+<h2 id=aomori>
+  <span class="img_style">
+    <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
+  </span>
+  <span class="img_style">
+    <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
+  </span>
+</h2>
+
+<h2 id=iwate>
+  <span class="img_style">
+    <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
+  </span>
+  <span class="img_style">
+    <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
+  </span>
+</h2>
+
+<h2 id=miyagi>
+  <span class="img_style">
+    <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
+  </span>
+  <span class="img_style">
+    <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
+  </span>
+</h2>
+
+<h2 id=akita>
   <span class="img_style">
     <img style="width:360px" src="img/goryokaku01.jpg"><br />五稜郭</a>
   </span>
