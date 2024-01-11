65a73e8f792b53ea79f32255fe15091fbee769b8
masashikuwahara <brucemclarenp4@gmail.com>
2024-01-12 00:01:54 +0900

    更新チェックページ追加

---
+++ edit/contact_check.php
@@ -0,0 +1,52 @@
+<?php
+require_once('library.php');
+session();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>修正内容確認</title>
+    <style>
+    .btn{
+        width: 100px;
+        height: 50px;
+        background-color: #00bfff;
+        border-radius: 20px;
+        border: none;
+        color: #ffffff;
+        }
+    .btn:hover {
+            background-color: #ed6fb5;
+        }
+    </style>
+</head>
+<body>
+    <?php
+
+    $post=($_POST);
+    $con_id=$post['id'];
+    $con_ck=$post['ck'];
+
+    if($con_ck=== NULL)
+    {
+        echo'<p style="color:#ff0000">チェックが入力されていません。</p><br />';
+        echo'<form>';
+        echo'<input class="btn" type="button" onclick="history.back()" value="戻る">';
+        echo'</form>';
+    }
+    else
+    {
+        echo'更新します';
+        echo'<br />';
+        echo'<form method="post" action="castles_edit_done.php">';
+        echo'<input type="hidden" name="id" value="'.$con_id.'">';
+        echo'<input type="hidden" name="cas" value="'.$con_ck.'">';
+        echo'<br />';
+        echo'<input class="btn" type="button" onclick="history.back()" value="戻る">&nbsp;';
+        echo'<input class="btn" type="submit" value="決定する">';
+        echo'</form>';
+    }
+    ?>
+</body>
+</html>
