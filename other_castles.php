c9aa6c36fe6b0a1e3e7ad8124d6d74a2f158a510
masashikuwahara <brucemclarenp4@gmail.com>
2024-01-20 20:23:31 +0900

    タイトル修正

--- other_castles.php
+++ other_castles.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <title>100名城</title>
+  <title>その他の城</title>
 </head>
 <body>
 <?php include('header.php'); ?>
