63a0c8150569b86d991bce33220b377b24ea0e8c
masashikuwahara <brucemclarenp4@gmail.com>
2024-01-20 20:23:42 +0900

    知識ページ更新

--- knowledge/index.php
+++ knowledge/index.php
@@ -4,11 +4,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../style.css">
   <title>城についての基礎知識</title>
 </head>
 <body>
-<?php include('header.php'); ?>
+<?php include('../header.php'); ?>
 
   <h1 class="title">城についての基礎知識</h1>
   <div class="wrapper">
@@ -69,4 +69,4 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script type="text/javascript" src="menu.js"></script>
-  <?php include('footer.php'); ?>
+  <?php include('../footer.php'); ?>
