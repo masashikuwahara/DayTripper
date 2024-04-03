74ecc4c3b1294204110a50c671cd57a683ed12e2
masashikuwahara <brucemclarenp4@gmail.com>
2024-04-03 23:49:06 +0900

    ログアウト後の画面遷移を変更
    ログアウト後にログイン画面へ遷移するように変更

--- edit/logout.php
+++ edit/logout.php
@@ -6,16 +6,7 @@ if (isset($_COOKIE[session_name()])==true)
     setcookie(session_name(),'',time()-42000,'/');
 }
 session_destroy();
+
+header('Location: login.html');
+
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>ログアウトしました</title>
-</head>
-<body>
-    ログアウトしました。<br />
-    <br />
-    <a href="login.html">ログイン画面へ</a>
-</body>
-</html>
