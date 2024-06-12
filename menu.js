aa407c830758caa50cdccc8782802e96be0f66e8
masashikuwahara <brucemclarenp4@gmail.com>
2024-06-13 00:53:25 +0900

    jsファイル変更
    jquery削除。

--- menu.js
+++ menu.js
@@ -1,6 +1,22 @@
-$(function() {
-  　$('.Toggle').click(function() {
-     $(this).toggleClass('active');
-    $('.menu').toggleClass('open');
-   });
-  });
+    // ハンバーガーボタン
+    let hamburgerBtn = document.getElementById('header-hamburger');
+    // ハンバーガーウィンドウ
+    let hamburgerWindw = document.getElementById('hamburger-window');
+    
+    /**
+     * ハンバーガーメニューの開閉を制御
+     */
+    hamburgerBtn.addEventListener('click', () => {
+      // ハンバーガーメニューのopenとcloseの状態を制御
+      if (hamburgerBtn.classList.contains('active')) {
+        hamburgerBtn.classList.remove('active');
+      } else {
+        hamburgerBtn.classList.add('active');
+      }
+      // ハンバーガーウィンドウの表示状態を制御
+      if (hamburgerWindw.classList.contains('open')) {
+        hamburgerWindw.classList.remove('open');
+      } else {
+        hamburgerWindw.classList.add('open');
+      }
+    });
