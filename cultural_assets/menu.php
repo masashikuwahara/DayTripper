0fc0d9e6fa4ef00ce86f07b1c40b48f145619bac
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-03 03:06:06 +0900

    メニュー作成

---
+++ cultural_assets/menu.php
@@ -0,0 +1,18 @@
+<header>
+  <div class="logo"><a href="index.html">DayTripper文化財編</a></div>
+    <a class="toggle-btn">
+      <span class="toggle-btn__line toggle-btn__line--top"></span>
+      <span class="toggle-btn__line toggle-btn__line--center"></span>
+      <span class="toggle-btn__line toggle-btn__line--bottom"></span>
+    </a>
+    <nav class="global-nav">
+      <div class="global-nav__inner">
+        <ul class="global-nav__list">
+          <li class="global-nav__item"><a href="about.html">このサイトについて</a></li>
+          <li class="global-nav__item"><a href="j-map.html">攻略した城</a></li>
+          <li class="global-nav__item"><a href="#">その他史跡等</a></li>
+          <li class="global-nav__item"><a href="#">訪問予定</a></li>
+        </ul>
+      </div>
+    </nav>
+</header>
