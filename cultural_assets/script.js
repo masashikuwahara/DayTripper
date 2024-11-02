5de41827acfff5ba47dab1bc863cb2d9ce50e24c
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-03 03:08:44 +0900

    トップページスクリプト作成
    アクセス時のローディング、アニメーション

---
+++ cultural_assets/script.js
@@ -0,0 +1,68 @@
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});
+
+//画像の設定
+
+var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
+		if (windowwidth > 768){
+			var responsiveImage = [//PC用の画像
+				{ src: 'img/img_01.jpg'},
+				{ src: 'img/img_02.jpg'},
+				{ src: 'img/img_03.jpg'},
+				{ src: 'img/img_04.jpg'},
+				{ src: 'img/img_05.jpg'},
+				{ src: 'img/img_06.jpg'},
+				{ src: 'img/img_07.jpg'}
+			];
+		} else {
+			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
+				{ src: 'img/img_01.jpg'},
+				{ src: 'img/img_02.jpg'},
+				{ src: 'img/img_03.jpg'},
+        { src: 'img/img_04.jpg'},
+				{ src: 'img/img_05.jpg'},
+				{ src: 'img/img_06.jpg'},
+				{ src: 'img/img_07.jpg'}
+			];
+		}
+
+//Vegas全体の設定
+
+$('#slider').vegas({
+		overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
+		transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
+		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
+		delay: 10000,//スライド間の遅延をミリ秒単位で。
+		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
+		animation: 'kenburns',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
+		slides: responsiveImage,//画像設定を読む
+		//timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
+	});
