e15b90c0ba1e1999bbf4085f784898d6ee9566d4
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-03 03:07:34 +0900

    メニュースクリプト作成

---
+++ cultural_assets/burger.js
@@ -0,0 +1,7 @@
+// メニュー
+jQuery(document).ready(function($){
+$('.toggle-btn').on('click',function(){
+    $('.toggle-btn__line').toggleClass('active');
+    $('.global-nav').fadeToggle();
+});
+});
