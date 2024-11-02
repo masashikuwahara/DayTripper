9c3eabf7debbe386798b2e49e3cd970516bd00bf
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-03 03:03:36 +0900

    文化財ページ作成
    文化財ページのみ分離

---
+++ cultural_assets/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="全国の文化財を廻る" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher" rel="stylesheet" />
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css">
+    <link href="style.css" rel="stylesheet" />
+    <title>DayTripper文化財偏</title>
+  </head>
+  <body>
+    <div id="splash">
+      <div id="splash_text"></div>
+    </div>
+    <div class="wrapper">
+      <div id="slider">
+        <?php include('menu.php'); ?>
+          <div class="home-content wrapper">
+            <h1 class="page-title">遺されたものたち</h1>
+            <p>-古の文化財と歴史スポット-</p>
+              <p>
+                こちらは番外編みたいなもので文化財を中心としたページです。<br>
+                城以外の寺社仏閣、古墳などを紹介しています。
+              </p>
+          </div>
+          <div class="icon">
+            <a href="https://twitter.com/sakamichiiwlu4e" target="blank"><i class="fab fa-twitter fa-3x"></i></a>
+            <a href="https://www.instagram.com/day_____tripper/" target="blank"><i class="fab fa-instagram fa-3x"></i></a>
+          </div>
+      </div>
+    </div>
+    <?php include('footer.php'); ?>
+    <script src="script.js"></script>
+    </body>
+</html>
