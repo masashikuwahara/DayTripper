dba3e820779dfe04a98cb851388512e3b7e487eb
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-05 00:09:45 +0900

    文化財ページの更新管理機能追加

---
+++ edit/update_c/update_add_done.php
@@ -0,0 +1,47 @@
+<?php
+require_once('../library.php');
+session();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>登録しました</title>
+</head>
+<body>
+    <?php
+    try
+    {
+        $post=($_POST);
+        $update_cas=$post['day'];
+        $update_information=$post['information'];
+        $update_id=$post['id'];
+        $update_kinds=$post['kinds'];
+        $update_url=$post['url'];
+
+        require('../../connect.php');
+
+        $sql = 'INSERT INTO info_c (day,content,number,kinds,url) 
+        VALUES (?,?,?,?,?)';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $update_cas;
+        $data[] = $update_information;
+        $data[] = $update_id;
+        $data[] = $update_kinds;
+        $data[] = $update_url;
+        $stmt->execute($data);
+
+        $dbh = null;
+        
+        $update_information = mb_convert_encoding($update_information, "UTF-8", "auto");
+        echo '追加しました。<br />';
+    }
+    catch (Exception $e)
+    {
+        echo('Error:'.$e->getMessage());
+        exit();
+    }
+    ?>
+    <a href="update_list.php">戻る</a>
+</body>
+</html>
---
+++ edit/update_c/update_delete_done.php
@@ -0,0 +1,36 @@
+<?php
+require_once('../library.php');
+session();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>削除しました</title>
+</head>
+<body>
+    <?php
+    try
+    {
+        $id=$_POST['id'];
+
+        require('../../connect.php');
+
+        $sql = 'DELETE FROM info WHERE id=?';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $id;
+        $stmt->execute($data);
+
+        $dbh = null;
+
+    }
+    catch (Exception $e)
+    {
+        echo('Error:'.$e->getMessage());
+        exit();
+    }
+    ?>
+    削除しました<br />
+    <a href="update_list.php">戻る</a>
+</body>
+</html>
