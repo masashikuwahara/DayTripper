be8bc49a6519f55f1b1b8441bc251f7b02c1f2b3
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-14 21:28:53 +0900

    猫ページ追加

---
+++ cultural_assets/cat.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Philosopher" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css">
+  <link rel="stylesheet" href="style.css">
+  <title>旅先で出会った猫</title>
+</head>
+<body>
+<?php include('menu.php'); ?>
+<div class="castle_page">
+  <h1 class="cas_title">旅先で出会った猫</h1>
+  <hr>
+  <?php
+    try
+    {
+      require('connect.php');
+      $dbh->query('SET NAMES utf8');
+      $sql='SELECT id,title,img1 FROM cultures ';
+      $stmt=$dbh->prepare($sql);
+      $stmt->execute();
+
+      $dbh = null;
+
+      while(true)
+      {
+        $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+        if(!$rec)
+        {
+          break;
+        }
+
+        if($rec['img1'] === '')
+        {
+          $img_name = '';
+        }
+        else
+        {
+          $img_name='<img style="width:360px" src="img/'.$rec['img1'].'">';
+        }
+
+        echo '<span class="img_style">'.
+            '<a href="culturals_detail.php?id='.$rec['id'].'">'.
+            $img_name.
+            '<br />'.
+            $rec['title'].
+              '</a>'.
+            '<br />'.
+            '</span>';
+      }
+
+      }
+      catch (Exception $e)
+      {
+        echo 'ただいま障害により大変ご迷惑をお掛けしております。';
+        exit();
+      }
+      ?>
+</div>
+<?php include('footer.php'); ?>
+</body>
+</html>
