c7a644f0a4c2e930396442834d8ea9e79b962a2a
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-16 16:04:33 +0900

    リンクを設定

--- edit/index.php
+++ edit/index.php
@@ -16,6 +16,8 @@ session();
     <br />
     <a href="culturalproperty/culturals_list.php">その他文化財</a><br />
     <br />
+    <a href="cats/">猫ページ管理(仮)</a><br />
+    <br />
     <a href="update/update_list.php">城郭更新情報</a><br />
     <br />
     <a href="update_c/update_list.php">文化財更新情報</a><br />
