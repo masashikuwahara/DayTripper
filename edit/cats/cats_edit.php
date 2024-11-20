ff33aba67ad06fbe3be98b5af352f73497d6a887
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-21 00:02:21 +0900

    編集ページ修正

--- edit/cats/cats_edit.php
+++ edit/cats/cats_edit.php
@@ -6,7 +6,7 @@ session();
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>記事修正</title>
+    <title>猫ページ修正</title>
     <style>
         .text{
             width: 600px;
@@ -39,53 +39,74 @@ session();
     <?php
     try{
 
-        $culturals_id=$_GET['id'];
+        $cat_id=$_GET['id'];
 
         require('../connect.php');
         $dbh->query('SET NAMES utf8');
-        $sql = 'SELECT title,year,specify,explan,access,
-        img1,img2,img3 FROM cultures WHERE id=?';
+        $sql = 'SELECT title, kind, color, feature, place, comment, 
+        img1, img2, img3, img4 ,img5 FROM cats WHERE id=?';
         $stmt = $dbh->prepare($sql);
-        $data[]=$culturals_id;
+        $data[]=$cat_id;
         $stmt->execute($data);
         
-        $cul =$stmt->fetch(PDO::FETCH_ASSOC);
-        $culturals_title=$cul['title'];
-        $culturals_year=$cul['year'];
-        $culturals_specify=$cul['specify'];
-        $culturals_explan=$cul['explan'];
-        $culturals_access=$cul['access'];
-        $culturals_img1_old=$cul['img1'];
-        $culturals_img2_old=$cul['img2'];
-        $culturals_img3_old=$cul['img3'];
+        $post =$stmt->fetch(PDO::FETCH_ASSOC);
+        $cat_title=$post['title'];
+        $cat_kind=$post['kind'];
+        $cat_color=$post['color'];
+        $cat_feature=$post['feature'];
+        $cat_place=$post['place'];
+        $cat_comment=$post['comment'];
+        $cat_img1_old=$post['img1'];
+        $cat_img2_old=$post['img2'];
+        $cat_img3_old=$post['img3'];
+        $cat_img4_old=$post['img4'];
+        $cat_img5_old=$post['img5'];
 
         $dbh = null;
 
-        if($culturals_img1_old === '')
+        if($cat_img1_old === '')
         {
             $disp_img='';
         }
         else
         {
-            $disp_img='<img src="../../img/'.$culturals_img1_old.'">';
+            $disp_img='<img src="../../cultural_assets/img/'.$cat_img1_old.'">';
         }
 
-        if($culturals_img2_old === '')
+        if($cat_img2_old === '')
         {
             $disp_img='';
         }
         else
         {
-            $disp_img='<img src="../../img/'.$culturals_img2_old.'">';
+            $disp_img='<img src="../../cultural_assets/img/'.$cat_img2_old.'">';
         }
 
-        if($culturals_img3_old === '')
+        if($cat_img3_old === '')
         {
             $disp_img='';
         }
         else
         {
-            $disp_img='<img src="../../img/'.$culturals_img3_old.'">';
+            $disp_img='<img src="../../cultural_assets/img/'.$cat_img3_old.'">';
+        }
+
+        if($cat_img4_old === '')
+        {
+            $disp_img='';
+        }
+        else
+        {
+            $disp_img='<img src="../../cultural_assets/img/'.$cat_img4_old.'">';
+        }
+
+        if($cat_img5_old === '')
+        {
+            $disp_img='';
+        }
+        else
+        {
+            $disp_img='<img src="../../cultural_assets/img/'.$cat_img5_old.'">';
         }
 
     }
@@ -100,29 +121,31 @@ session();
     記事修正<br />
     <br />
     文化財名<br />
-    <?php echo $culturals_title;?>
+    <?php echo $cat_title;?>
     <br />
     <br />
 
-    <form method="post" action="culturals_edit_check.php" enctype="multipart/form-data">
-      <input type="hidden" name="id" value="<?php echo $culturals_id; ?>">
-      <input name="img_name_old1" type="hidden" value="<?php echo $culturals_img1_old;?>">
-      <input name="img_name_old2" type="hidden" value="<?php echo $culturals_img2_old;?>">
-      <input name="img_name_old3" type="hidden" value="<?php echo $culturals_img3_old;?>">
-        文化財の名称を入力してください。<br />
-        <input class="tex" type="text" name="title" value="<?php echo $culturals_title; ?>"><br />
-        作成年を入力してください。<br />
-        <input class="tex" type="text" name="year" value="<?php echo $culturals_year; ?>"><br />
-        指定文化財を入力してください。<br />
-        <input class="tex" type="text" name="specify" value="<?php echo $culturals_specify; ?>"><br />
-        説明を入力してください。<br />
-        <textarea class="textb" type="text" name="explan" ><?php echo $culturals_explan; ?></textarea><br />
-        アクセスを入力してください。<br />
-        <input class="texta" type="text" name="access" ><br />
+    <form method="post" action="cat_edit_check.php" enctype="multipart/form-data">
+      <input type="hidden" name="id" value="<?php echo $cat_id; ?>">
+      <input name="img_name_old1" type="hidden" value="<?php echo $cat_img1_old;?>">
+      <input name="img_name_old2" type="hidden" value="<?php echo $cat_img2_old;?>">
+      <input name="img_name_old3" type="hidden" value="<?php echo $cat_img3_old;?>">
+      <input name="img_name_old3" type="hidden" value="<?php echo $cat_img4_old;?>">
+      <input name="img_name_old3" type="hidden" value="<?php echo $cat_img5_old;?>">
+        毛色・色柄<br />
+        <input class="tex" type="text" name="color" value="<?php echo $cat_title; ?>"><br />
+        特徴<br />
+        <input class="tex" type="text" name="feature" value="<?php echo $cat_feature; ?>"><br />
+        目撃場所<br />
+        <input class="tex" type="text" name="place" value="<?php echo $cat_place; ?>"><br />
+        一言<br />
+        <textarea class="textb" type="text" name="explan" ><?php echo $cat_comment; ?></textarea><br />
         画像を選んでください。<br />
         <input type="file" name="img1" ><br /><br />
         <input type="file" name="img2" ><br /><br />
         <input type="file" name="img3" ><br /><br />
+        <input type="file" name="img4" ><br /><br />
+        <input type="file" name="img5" ><br /><br />
       <input class="btn" type="button" onclick="history.back()"value="戻る">
       <input class="btn" type="submit" value="次のページへ">
     </form>
