b47d2161bd16ec546f3666641a40ebefe8babcaf
masashikuwahara <brucemclarenp4@gmail.com>
2024-11-22 23:05:25 +0900

    編集ページ修正

--- edit/cats/cats_edit_done.php
+++ edit/cats/cats_edit_done.php
@@ -13,63 +13,86 @@ session();
     try
     {
         $post=($_POST);
-        $culturals_id=$post['id'];
-        $culturals_title=$post['title'];
-        $culturals_year=$post['year'];
-        $culturals_specify=$post['specify'];
-        $culturals_explan=$post['explan'];
-        $culturals_access=$post['access'];
+        $cat_id=$post['id'];
+        $cat_title=$post['title'];
+        $cat_kind=$post['kind'];
+        $cat_color=$post['color'];
+        $cat_feature=$post['feature'];
+        $cat_place=$post['place'];
+        $cat_comment=$post['comment'];
         $img_name1_old=$post['img_name1_old'];
         $img_name2_old=$post['img_name2_old'];
         $img_name3_old=$post['img_name3_old'];
-        $culturals_img1=$post['img1'];
-        $culturals_img2=$post['img2'];
-        $culturals_img3=$post['img3'];
+        $img_name4_old=$post['img_name4_old'];
+        $img_name5_old=$post['img_name5_old'];
+        $cat_img1=$post['img1'];
+        $cat_img2=$post['img2'];
+        $cat_img3=$post['img3'];
+        $cat_img4=$post['img4'];
+        $cat_img5=$post['img5'];
 
         require('../connect.php');
 
-        $sql = 'UPDATE cultures SET title=?,year=?,specify=?,explan=?,
-        access=?,img1=?,img2=?,img3=? WHERE id=?';
+        $sql = 'UPDATE cats SET title=?, kind=?, color=?, feature=?,
+        place=?, comment=?, img1=?, img2=?, img3=?, img4=?, img5=? WHERE id=?';
         $stmt = $dbh->prepare($sql);
-        $data[] = $culturals_title;
-        $data[] = $culturals_year;
-        $data[] = $culturals_specify;
-        $data[] = $culturals_explan;
-        $data[] = $culturals_access;
-        $data[] = $culturals_img1;
-        $data[] = $culturals_img2;
-        $data[] = $culturals_img3;
-        $data[] = $culturals_id;
+        $data[] = $cat_title;
+        $data[] = $cat_kind;
+        $data[] = $cat_color;
+        $data[] = $cat_feature;
+        $data[] = $cat_place;
+        $data[] = $cat_img1;
+        $data[] = $cat_img2;
+        $data[] = $cat_img3;
+        $data[] = $cat_img4;
+        $data[] = $cat_img5;
+        $data[] = $cat_id;
         $stmt->execute($data);
 
         $dbh = null;
 
-        if($img_name1_old !=$culturals_img1)
+        if($img_name1_old !=$cat_img1)
         {
             if($img_name1_old !='')
             {
-                unlink('../../img/'.$img_name1_old);
+                unlink('../../cultural_assets/img/'.$img_name1_old);
             }
         }
 
-        if($img_name2_old !=$culturals_img2)
+        if($img_name2_old !=$cat_img2)
         {
             if($img_name2_old !='')
             {
-                unlink('../../img/'.$img_name2_old);
+                unlink('../../cultural_assets/img/'.$img_name2_old);
             }
         }
 
-        if($img_name3_old !=$culturals_img3)
+        if($img_name3_old !=$cat_img3)
         {
             if($img_name3_old !='')
             {
-                unlink('../../img/'.$img_name3_old);
+                unlink('../../cultural_assets/img/'.$img_name3_old);
+            }
+        }
+
+        if($img_name4_old !=$cat_img4)
+        {
+            if($img_name3_old !='')
+            {
+                unlink('../../cultural_assets/img/'.$img_name4_old);
+            }
+        }
+
+        if($img_name5_old !=$cat_img5)
+        {
+            if($img_name5_old !='')
+            {
+                unlink('../../cultural_assets/img/'.$img_name5_old);
             }
         }
         
-        $culturals_title = mb_convert_encoding($culturals_title, "UTF-8", "auto");
-        echo $culturals_title.'を修正しました。<br />';
+        $cat_title = mb_convert_encoding($cat_title, "UTF-8", "auto");
+        echo $cat_title.'を修正しました。<br />';
     }
     catch (Exception $e)
     {
@@ -77,6 +100,6 @@ session();
         exit();
     }
     ?>
-    <a href="culturals_list.php">戻る</a>
+    <a href="index.php">戻る</a>
 </body>
 </html>
