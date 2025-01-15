a527f8d923fa1c8ee6e4870d430d8b51b62f477f
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-15 23:05:19 +0900

    リンク変更

--- edit/index.php
+++ edit/index.php
@@ -12,17 +12,17 @@ session();
 <body>
     編集管理トップメニュー<br />
     <br />
-    <a href="castles/castles_list.php">城管理</a><br />
+    <a href="castles/">城管理</a><br />
     <br />
-    <a href="culturalproperty/culturals_list.php">その他文化財</a><br />
+    <a href="culturalproperty/">その他文化財</a><br />
     <br />
     <a href="cats/">猫ページ管理</a><br />
     <br />
     <a href="#">VRページ</a><br />
     <br />
-    <a href="update/update_list.php">城郭更新情報</a><br />
+    <a href="update/">城郭更新情報</a><br />
     <br />
-    <a href="update_c/update_list.php">文化財更新情報</a><br />
+    <a href="update_c/">文化財更新情報</a><br />
     <br />
     <a href="contact/">お問い合わせ</a><br />
     <br />
