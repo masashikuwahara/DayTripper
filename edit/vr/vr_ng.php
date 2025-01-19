7e1dba06847057d55dc7b53071295f1c97c712d9
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-20 00:05:41 +0900

    ngページを修正

--- edit/vr/vr_ng.php
+++ edit/vr/vr_ng.php
@@ -5,11 +5,12 @@ session();
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>選択されていません</title>
 </head>
 <body>
-    城が選択されていません。 <br />
+    動画が選択されていません。 <br />
     <br />
-    <a href="castles_list.php">戻る</a>
+    <a href="index.php">戻る</a>
 </body>
 </html>
