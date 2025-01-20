fcde15e6f0847b52718f363c6eff878e602429db
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-21 00:01:10 +0900

    更新情報管理ページ修正

--- edit/update_c/update_add.php
+++ edit/update_c/update_add.php
@@ -37,18 +37,18 @@ session();
 <div class="form">
   <h1>更新情報追加</h1>
   <form method="post" action="update_add_check.php" enctype="multipart/form-data">
-    更新日時を入力してください。yyyy.mm.dd<br />
+    更新日時を入力してください。yyyy.mm.dd形式で入力。<br />
     <input class="tex" type="text" name="day" ><br />
     更新内容を入力してください。<br />
     <input class="tex" type="text" name="information" ><br />
     idを入力してください。ページの更新の場合は空欄。<br />
     <input class="tex" type="text" name="id" ><br />
-    真偽値を入力してください。1:文化財 0:ページ更新<br />
+    真偽値を入力してください。1:文化財 0:ページ更新(猫ページ更新もこっち)<br />
     <select name="kinds" id="" class="tex">
       <option>1</option>
       <option>0</option>
     </select><br />
-    ページ更新の場合はURLを入力してください<br />
+    ページ更新の場合はURLを入力してください。(猫ページ更新もこっち)<br />
     <input class="tex" type="text" name="url" ><br />
     <input class="btn" type="button" onclick="history.back()"value="戻る">
     <input class="btn" type="submit" value="次のページへ">
