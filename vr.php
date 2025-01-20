b3f3f994e1305fccf817ee12cbd35e6038235d09
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-21 00:01:45 +0900

    vrページ修正

--- vr.php
+++ vr.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <title>VR</title>
+  <title>VR動画</title>
   <style>
     span {
       width: 360px;
@@ -15,7 +15,7 @@
 <body>
 <?php include('header.php'); ?>
 <div class="castle_page">
-  <h1 class="title">VR</h1>
+  <h1 class="title">VR動画</h1>
   
   <div class="about">
   </div>
