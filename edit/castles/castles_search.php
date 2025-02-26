71d0828eaa01533ee8d3d19d3ba840b21baa5f85
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-27 00:31:51 +0900

    検索システム改修

--- edit/castles/castles_search.php
+++ edit/castles/castles_search.php
@@ -16,45 +16,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <title><?php echo $s ?>の検索結果</title>
-    <style>
-    .btn{
-          width: 50px;
-          height: 30px;
-          background-color: #00bfff;
-          border-radius: 20px;
-          border: none;
-          color: #ffffff;
-          }
-
-    .btn:hover {
-              background-color: #ed6fb5;
-          }
-
-    .add{
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      line-height: 1;
-      text-decoration: none;
-      color: #ffffff;
-      border-radius: 20px;
-      border: none;
-      width: 120px;
-      height: 40px;
-      transition: 0.3s;
-      background-image: radial-gradient(circle at 100% 0%, 
-      rgba(94, 138, 243, 1) 15%, rgba(243, 61, 223, 1));
-    }
-
-    .add:hover {
-      opacity: .5;
-          }
-	</style>
 </head>
+<style>
+  .result {
+    color: red;
+  }
+</style>
 <body>
-  <div class="search">
+<header>
+  <h1>検索結果</h1>
+</header>
+  <main>
     <?php
     echo '<form method="post" action="castles_branch.php">';
     if($s != ''){
@@ -71,24 +45,27 @@
             echo '<input type="radio" name="id" value="'.$r['id'].'">';
             echo $r['title']. '<br>';
           } 
+          echo '<br/>';
+          echo '<input class="btn" type="submit" name="disp" value="参照">';
+          echo '<input class="btn" type="submit" name="edit" value="修正">';
+          echo '<input disabled class="btn" type="submit" name="delete" value="削除">';
         }else{
           echo '<div class="result">そのキーワードでは見つかりませんでした</div>';
         }
     }else{
       echo '<div class="result">キーワードを入力してください</div>';
     }
-    echo '<br/>';
-    echo '<br/>';
-    echo '<input class="btn" type="submit" name="disp" value="参照">';
-    echo '<input class="btn" type="submit" name="edit" value="修正">';
-    echo '<input disabled class="btn" type="submit" name="delete" value="削除">';
     echo '</form>';
     ?>
-    <div>もう一度検索する</div>
+    <H2>もう一度検索する</H2>
     <form action="castles_search.php" method="get">
       <input class="sea" type="text" name="s" placeholder="例:姫路城">
       <input class="btn" type="submit" value="検索">
     </form>
-
-    <a href="castles_list.php">一覧へ戻る</a><br />
-  </div>
+    <p class="btn-group"><a href="index.php" class="btn">トップメニューへ</a></p>
+  </main>
+  <footer>
+    <p>&copy; 2025 城検索システム</p>
+  </footer>
+  </body>
+</html>
