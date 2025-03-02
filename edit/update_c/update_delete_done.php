b8ca40d60ab9f08606b217805bb7d17edd6205e0
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-02 12:35:29 +0900

    文化財更新情報削除感完了ページ

--- edit/update_c/update_delete_done.php
+++ edit/update_c/update_delete_done.php
@@ -6,6 +6,8 @@ session();
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>削除しました</title>
 </head>
 <body>
@@ -16,7 +18,7 @@ session();
 
         require('../../connect.php');
 
-        $sql = 'DELETE FROM info WHERE id=?';
+        $sql = 'DELETE FROM info_c WHERE id=?';
         $stmt = $dbh->prepare($sql);
         $data[] = $id;
         $stmt->execute($data);
@@ -30,7 +32,15 @@ session();
         exit();
     }
     ?>
-    削除しました<br />
-    <a href="index.php">戻る</a>
+    <header>
+      <h1>削除完了</h1>
+    </header>
+    <main>
+        <div class="content">
+            削除しました
+        </div>
+    <p class="btn-group"><a href="index.php" class="btn">トップメニューへ</a></p>
+    </main>
+    <?php include("../footer.php") ?>
 </body>
 </html>
