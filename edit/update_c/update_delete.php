88ebd65499b482cb3aef0b408fee6465ebd9f606
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-02 12:34:57 +0900

    文化財削除確認ページ修正

--- edit/update_c/update_delete.php
+++ edit/update_c/update_delete.php
@@ -6,20 +6,9 @@ session();
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>削除内容確認</title>
-    <style>
-    .btn{
-        width: 100px;
-        height: 50px;
-        background-color: #00bfff;
-        border-radius: 20px;
-        border: none;
-        color: #ffffff;
-        }
-    .btn:hover {
-            background-color: #ed6fb5;
-        }
-    </style>
 </head>
 <body>
   <?php
@@ -29,7 +18,7 @@ session();
 
         require('../../connect.php');
         $dbh->query('SET NAMES utf8');
-        $sql = 'SELECT * FROM info WHERE id=?';
+        $sql = 'SELECT * FROM info_c WHERE id=?';
         $stmt = $dbh->prepare($sql);
         $data[]=$update_id;
         $stmt->execute($data);
@@ -37,7 +26,7 @@ session();
         $info =$stmt->fetch(PDO::FETCH_ASSOC);
         $update_id=$info['id'];
         $update_day=$info['day'];
-        $update_info=$info['information'];
+        $update_content=$info['content'];
 
         $dbh = null;
       }
@@ -47,21 +36,27 @@ session();
         exit();
       }
    ?>
-
-  削除内容確認<br />
+    <header>
+       <h1>削除内容確認</h1>
+    </header>
+   <main>
+    <div class="content">
   <br />
   更新日<br />
   <?php echo $update_day; ?>
   <br />
   更新内容<br />
-  <?php echo $update_info; ?>
+  <?php echo $update_content; ?>
   <br />
   <br />
   この内容を削除してよろしいですか？<br />
+  </div>
     <form method="post" action="update_delete_done.php">
       <input type="hidden" name="id" value="<?php echo $update_id ?>">
         <input class="btn" type="button" onclick="history.back()" value="戻る">&nbsp;
         <input class="btn" type="submit" value="削除する">
     </form>
+  </main>
+  <?php include("../footer.php"); ?>
 </body>
 </html>
