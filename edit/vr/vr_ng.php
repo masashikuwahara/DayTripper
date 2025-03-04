807723f1715c715ba0f0aadfa42a1097721d0a74
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-05 00:27:25 +0900

    NGページ改修

--- edit/vr/vr_ng.php
+++ edit/vr/vr_ng.php
@@ -6,11 +6,18 @@ session();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>選択されていません</title>
 </head>
 <body>
-    動画が選択されていません。 <br />
-    <br />
-    <a href="index.php">戻る</a>
+<header>
+    <h1>選択されていません</h1>
+</header>
+<main>
+    <div class="btn-group">
+        <input class="btn" type="button" onclick="history.back()" value="戻る">
+    </div>
+</main>
+<?php include("../footer.php") ?>
 </body>
 </html>
