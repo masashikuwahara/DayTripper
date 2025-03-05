665aea58165f241993356e5e69421332b715f976
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-06 00:41:07 +0900

    城管理ページ修正

--- edit/castles/castles_disp.php
+++ edit/castles/castles_disp.php
@@ -44,144 +44,144 @@ $castles_img5=$cas['img5'];
         <h1><?php echo $castles_title; ?></h1>
     </header>
     <main>
-    <?php
-    try{
+        <?php
+        try{
 
-        $dbh = null;
+            $dbh = null;
 
-        if($castles_img1 === '')
-        {
-            $disp_img1='';
+            if($castles_img1 === '')
+            {
+                $disp_img1='';
+            }
+            else
+            {
+                $disp_img1='<img src="../../img/'.$castles_img1.'" class="pic" >';
+            }
         }
-        else
+        catch (Exception $e)
         {
-            $disp_img1='<img src="../../img/'.$castles_img1.'" class="pic" >';
+            echo'ただいま障害により大変ご迷惑をおかけしております。';
+            exit();
         }
-    }
-    catch (Exception $e)
-    {
-        echo'ただいま障害により大変ご迷惑をおかけしております。';
-        exit();
-    }
 
-    try{
+        try{
 
-        $dbh = null;
+            $dbh = null;
 
-        if($castles_img2 === '')
+            if($castles_img2 === '')
+            {
+                $disp_img2='';
+            }
+            else
+            {
+                $disp_img2='<img src="../../img/'.$castles_img2.'" class="pic">';
+            }
+        }
+        catch (Exception $e)
         {
-            $disp_img2='';
+            echo'ただいま障害により大変ご迷惑をおかけしております。';
+            exit();
         }
-        else
+
+        try{
+
+            $dbh = null;
+
+            if($castles_img3 === '')
+            {
+                $disp_img3='';
+            }
+            else
+            {
+                $disp_img3='<img src="../../img/'.$castles_img3.'" class="pic">';
+            }
+        }
+        catch (Exception $e)
         {
-            $disp_img2='<img src="../../img/'.$castles_img2.'" class="pic">';
+            echo'ただいま障害により大変ご迷惑をおかけしております。';
+            exit();
         }
-    }
-    catch (Exception $e)
-    {
-        echo'ただいま障害により大変ご迷惑をおかけしております。';
-        exit();
-    }
 
-    try{
+        try{
 
-        $dbh = null;
+            $dbh = null;
 
-        if($castles_img3 === '')
+            if($castles_img4 === '')
+            {
+                $disp_img4='';
+            }
+            else
+            {
+                $disp_img4='<img src="../../img/'.$castles_img4.'" class="pic">';
+            }
+        }
+        catch (Exception $e)
         {
-            $disp_img3='';
+            echo'ただいま障害により大変ご迷惑をおかけしております。';
+            exit();
         }
-        else
+
+        try{
+
+            $dbh = null;
+
+            if($castles_img5 === '')
+            {
+                $disp_img5='';
+            }
+            else
+            {
+                $disp_img5='<img src="../../img/'.$castles_img5.'" class="pic">';
+            }
+        }
+        catch (Exception $e)
         {
-            $disp_img3='<img src="../../img/'.$castles_img3.'" class="pic">';
+            echo'ただいま障害により大変ご迷惑をおかけしております。';
+            exit();
         }
-    }
-    catch (Exception $e)
-    {
-        echo'ただいま障害により大変ご迷惑をおかけしております。';
-        exit();
-    }
 
-    try{
-
-        $dbh = null;
-
-        if($castles_img4 === '')
-        {
-            $disp_img4='';
-        }
-        else
-        {
-            $disp_img4='<img src="../../img/'.$castles_img4.'" class="pic">';
-        }
-    }
-    catch (Exception $e)
-    {
-        echo'ただいま障害により大変ご迷惑をおかけしております。';
-        exit();
-    }
-
-    try{
-
-        $dbh = null;
-
-        if($castles_img5 === '')
-        {
-            $disp_img5='';
-        }
-        else
-        {
-            $disp_img5='<img src="../../img/'.$castles_img5.'" class="pic">';
-        }
-    }
-    catch (Exception $e)
-    {
-        echo'ただいま障害により大変ご迷惑をおかけしております。';
-        exit();
-    }
-
-    ?>
-    <?php echo $disp_img1;?>
-    <?php echo $disp_img2;?>
-    <?php echo $disp_img3;?>
-    <?php echo $disp_img4;?>
-    <?php echo $disp_img5;?>
-    <h2>城郭構造</h2>
-    <div class="content">
-        <?php echo $castles_structure;?>
-        <br />
-        <h2>天守構造</h2>
-        <?php echo $castles_tenshu;?>
-        <br />
-        <h2>築城主</h2>
-        <?php echo $castles_builder;?>
-        <br />
-        <h2>築城年</h2>
-        <?php echo $castles_year;?>
-        <br />
-        <h2>主な城主</h2>
-        <?php echo $castles_lord;?>
-        <br />
-        <h2>遺構</h2>
-        <?php echo $castles_remains;?>
-        <br />
-        <h2>指定文化財</h2>
-        <?php echo $castles_specify1;?>
-        <br />
-        <h2>おすすめ度</h2>
-        <?php echo $castles_recommend;?>
-        <br />
-        <h2>解説</h2>
-        <?php echo $castles_explan;?>
-        <br />
-        <h2>アクセス</h2>
-        <?php echo $castles_access;?>
-        <br />
-   </div>
-    <form>
-    <input class="btn" type="button" onclick="history.back()" value="戻る">
-    </form>
+        ?>
+        <?php echo $disp_img1;?>
+        <?php echo $disp_img2;?>
+        <?php echo $disp_img3;?>
+        <?php echo $disp_img4;?>
+        <?php echo $disp_img5;?>
+        <h2>城郭構造</h2>
+            <div class="content">
+                <?php echo $castles_structure;?>
+                <br />
+                <h2>天守構造</h2>
+                <?php echo $castles_tenshu;?>
+                <br />
+                <h2>築城主</h2>
+                <?php echo $castles_builder;?>
+                <br />
+                <h2>築城年</h2>
+                <?php echo $castles_year;?>
+                <br />
+                <h2>主な城主</h2>
+                <?php echo $castles_lord;?>
+                <br />
+                <h2>遺構</h2>
+                <?php echo $castles_remains;?>
+                <br />
+                <h2>指定文化財</h2>
+                <?php echo $castles_specify1;?>
+                <br />
+                <h2>おすすめ度</h2>
+                <?php echo $castles_recommend;?>
+                <br />
+                <h2>解説</h2>
+                <?php echo $castles_explan;?>
+                <br />
+                <h2>アクセス</h2>
+                <?php echo $castles_access;?>
+                <br />
+            </div>
+        <form>
+            <input class="btn" type="button" onclick="history.back()" value="戻る">
+        </form>
     </main>
-    <?php include("../footer.php") ?>
+<?php include("../footer.php") ?>
 </body>
 </html>
