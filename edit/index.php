13fd70f6c5a55e8bdc349bade62ee66806a1f25a
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-06 00:41:38 +0900

    城管理トップページfooter改修

--- edit/index.php
+++ edit/index.php
@@ -29,6 +29,7 @@ $userName = $_SESSION['user_name'] ?? 'ゲスト';
             <li><a href="logout.php">ログアウト</a></li>
         </ul>
     </main>
-    <?php include("../footer.php") ?>
+    System version 2.0.0
+    <?php include("footer.php") ?>
 </body>
 </html>
